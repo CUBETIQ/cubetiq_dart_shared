c1437f133a3ec6c715b87ed572dc8c3ab1a41bd5
Sambo Chea <sombochea@cubetiqs.com>
2021-06-01 19:41:14 +0700

    init dart shared

---
+++ example/cubetiq_dart_shared_example.dart
@@ -0,0 +1,6 @@
+import 'package:cubetiq_dart_shared/cubetiq_dart_shared.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/cubetiq_dart_shared_test.dart
@@ -0,0 +1,16 @@
+import 'package:cubetiq_dart_shared/cubetiq_dart_shared.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
